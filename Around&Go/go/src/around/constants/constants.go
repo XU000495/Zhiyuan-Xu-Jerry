5ba4182eebc326b03b2b4aa81a10f9261c68a655
XU000495 <60242421+XU000495@users.noreply.github.com>
2024-08-27 19:27:02 +0800

    Add files via upload

---
+++ Around&Go/go/src/around/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+    USER_INDEX = "user"
+    POST_INDEX = "post"
+    ES_URL = "http://10.128.0.2:9200"
+    ES_USERNAME = "ikun114514"
+    ES_PASSWORD = "ikun114514"
+    GCS_BUCKET = "xuzhiyuan-bucket"
+)
